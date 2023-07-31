8903360eedb2e17f8f9d688920b050294caaef35
이제일 <dlwpdlf147@naver.com>
2023-07-31 05:02:56 +0000

    change project name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'c_shop.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ssim-zang.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- ssim-zang/wsgi.py
+++ ssim-zang/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for c_shop project.
+WSGI config for ssim-zang project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'c_shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ssim-zang.settings')
 
 application = get_wsgi_application()
